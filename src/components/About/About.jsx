3dc45b3dbc8134ea5676cf88093e7a6d104f6f84
nayebislam <nayebislamnafi475@gmail.com>
2025-05-11 23:49:43 +0600

    about part update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Container from '../layout/Container'
 import aboutimage from '../../assets/aboutimage.png'
+import aboutimage1 from '../../assets/aboutimage1.png'
+
 
 const About = () => {
     return (
         <section className='py-[200px] bg-[#F4FAFA]'>
             <Container>
-                <div>
+                <div className='flex justify-between items-center'>
                     <div>
                         <h1 className='max-w-[594px] font-primary font-bold text-[46px] leading-[58px] text-titleColor'>What they are talking about us?</h1>
                         <p className='max-w-[503px] font-secondary font-medium text-[18px] leading-[32px] text-peraColor mt-[20px] mb-[40px]'>testimonials are evidence of customers talking about your product or service in their own words. At their best, they are authentic, descriptive.</p>
@@ -30,7 +32,23 @@ const About = () => {
                             </div>
                         </div>
                     </div>
-                    <div></div>
+                    <div className='about_part py-[57px] px-[60px] bg-white rounded-[20px] relative'>
+                        <img className='absolute left-[48px] top-[-96px]' src={aboutimage1} alt="" />
+                        <p className='max-w-[567px] font-secondary text-[22px] leading-[44px] text-peraColor'>“Wow. Amazing company amazing Expert Manager. They know how to get things done when it comes to online trading.</p>
+                        <div className='flex justify-between items-end mt-[37px]'>
+                            <div>
+                                <h1 className='font-primary font-bold text-[24px] leading-[36px] text-[#30344E]'>Robert Fox</h1>
+                                <p className='font-secondary text-[16px] leading-[26px] text-[#30344E] mt-[4px]'>Digital Marketer</p>
+                            </div>
+                            <div className='flex gap-[3px] text-[#FDB400]'>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </Container>
         </section>
