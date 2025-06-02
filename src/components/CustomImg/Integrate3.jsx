2dd74a6c302a9bddeb81d2cda8bea8a6f9b5e34b
nayebislam <nayebislamnafi475@gmail.com>
2025-06-02 23:51:59 +0600

    integrate part responsive update

---
+++ src/components/CustomImg/Integrate3.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Integrate3 = () => {
+    return (
+        <div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="151"
+                height="142"
+                viewBox="0 0 151 142"
+                preserveAspectRatio="xMidYMid meet"
+                fill="none"
+                className="group w-[70px]  md:w-[151px] h-auto  cursor-pointer custom_shadow  "
+            >
+                <rect width="151" height="142" rx="11.4932" fill="white" />
+                <path className="group-hover:fill-[#43E7DF] duration-200 "
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M46.0125 72.7433L61.425 57.3345V55.8349H48.7875L51.15 59.9214L43.2 67.907L30 45H72.3V61.8335L59.5875 74.5429L62.4 79.3792L75 66.7448V66.8198V66.7448L87.6375 79.3792L90.45 74.5429L77.7375 61.8335V45H120L106.8 67.907L98.85 59.9589L101.213 55.8724H88.575V57.372L103.987 72.7808C99.3375 80.8414 94.65 88.9394 90 97L75 82.1536L60 96.9625C55.35 88.9019 50.7 80.8414 46.0125 72.7433Z"
+                    fill="#29313D"
+                    fill-opacity="0.8"
+                />
+            </svg>
+        </div>
+    )
+}
+
+export default Integrate3
