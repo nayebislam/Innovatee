47408498eb793ae156715307107d3071741451b6
nayebislam <nayebislamnafi475@gmail.com>
2025-06-03 02:52:21 +0600

    navbar responsive update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -21,33 +21,35 @@ const Navbar = () => {
                 
             </div> */}
             <Container>
-                <div className='xl:flex justify-between items-center'>
+                <div className='md:flex justify-between items-center'>
                     <div className='flex justify-between items-center'>
-                        <img src={logo} alt="#logo" />
+                        <a href="#">
+                            <img src={logo} alt="#logo" />
+                        </a>
                         <div>
-                            <FaBars onClick={handleClic} className='block xl:hidden text-2xl text-white' />
+                            <FaBars onClick={handleClic} className='block md:hidden text-2xl text-white' />
                         </div>
                     </div>
                     <div>
-                        <ul className='hidden xl:flex gap-[50px] font-secondary text-lg font-medium text-white'>
-                            <li>
-                                <a href="#">Home</a>
-                            </li>
-                            <li>
-                                <a href="#">Features</a>
-                            </li>
-                            <li>
-                                <a href="#">Service</a>
-                            </li>
-                            <li>
-                                <a href="#">Pages</a>
-                            </li>
-                            <li>
-                                <a href="#">Blog</a>
-                            </li>
-                        </ul>
+                        <ul className="hidden flex-col md:flex-row md:flex  mt-5 md:mt-0 gap-x-[68px]  items-center font-secondary text-white text-[16px] md:text-lg font-medium ">
+						<li className="mt-2 md:mt-0">
+							<a href="#">Home</a>
+						</li>
+						<li className="mt-2 md:mt-0">
+							<a href="#">Features</a>
+						</li>
+						<li className="mt-2 md:mt-0">
+							<a href="#">Service</a>
+						</li>
+						<li className="mt-2 md:mt-0">
+							<a href="#">Pages</a>
+						</li>
+						<li className="mt-2 md:mt-0">
+							<a href="#">Blog</a>
+						</li>
+					</ul>
                     </div>
-                    <div className='hidden xl:flex items-center gap-x-9'>
+                    <div className='hidden md:flex items-center gap-x-9'>
                         <button className='font-secondary text-lg text-white font-medium cursor-pointer'>Login</button>
                         <Button>Register</Button>
                     </div>
@@ -56,33 +58,33 @@ const Navbar = () => {
 
             {
                 show &&
-                <div className='flex flex-col justify-center items-center'>
+                <div className='relative flex flex-col justify-center items-center'>
                     <div>
-                        <RxCross2 onClick={() => setShow(false)} className='text-2xl text-white' />
+                        <RxCross2 onClick={() => setShow(false)} className='block absolute top-[-42px] right-5 bg-primary md:hidden text-2xl text-white cursor-pointer' />
                     </div>
                     <div>
-                        <ul className='gap-[50px] font-secondary text-lg font-medium text-white'>
-                            <li>
-                                <a href="#">Home</a>
-                            </li>
-                            <li>
-                                <a href="#">Features</a>
-                            </li>
-                            <li>
-                                <a href="#">Service</a>
-                            </li>
-                            <li>
-                                <a href="#">Pages</a>
-                            </li>
-                            <li>
-                                <a href="#">Blog</a>
-                            </li>
-                        </ul>
+                        <ul className="flex-col md:flex-row md:flex  mt-5 md:mt-0 gap-x-[68px]  items-center font-secondary text-white text-[16px] md:text-lg font-medium ">
+						<li className="mt-2 md:mt-0">
+							<a href="#">Home</a>
+						</li>
+						<li className="mt-2 md:mt-0">
+							<a href="#">Features</a>
+						</li>
+						<li className="mt-2 md:mt-0">
+							<a href="#">Service</a>
+						</li>
+						<li className="mt-2 md:mt-0">
+							<a href="#">Pages</a>
+						</li>
+						<li className="mt-2 md:mt-0">
+							<a href="#">Blog</a>
+						</li>
+					</ul>
                     </div>
-                    <div className='items-center gap-y-9'>
+                    <div className='text-white mt-7 md:mt-0 justify-between md:justify-normal flex gap-6 md:gap-9 items-center text-[16px] md:text-lg'>
                         <button className='font-secondary text-lg text-white font-medium cursor-pointer'>Login</button>
                         <Button>Register</Button>
-                    </div>
+                    </div> 
                 </div>
             }
 
