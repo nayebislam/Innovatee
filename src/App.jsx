dee5e6e6e1174560aed7e54c6a5abd0b5381b605
nayebislam <nayebislamnafi475@gmail.com>
2025-06-08 03:26:45 +0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Banner from './components/Banner/Banner'
+import Banner from './components/banner/Banner.jsx'
 import Navbar from './components/Navbar/Navbar'
 import Header from './components/Header/Header'
 import Platform from './components/Platform/Platform'
